9e7b9a208c029e4dba3f13345b6153116c12b5e8
Hao Xiong <haoxiong@outlook.com>
2017-03-24 04:23:52 -0500

    still 50% changes that w=0
    Better check the value of w

--- src/CollaborativeFiltering.java
+++ src/CollaborativeFiltering.java
@@ -236,8 +236,10 @@ class User {
                 //if the user has ever rated this movie
                 if (pos > -1) {
                     double w = CollaborativeFiltering.getWeight(index, user.index);
-                    norm += Math.abs(w);
-                    result += w * user.dRatings[pos];
+                    if(w != 0) {
+                      norm += Math.abs(w);
+                      result += w * user.dRatings[pos];
+                    }
                 }
             }
             pRatings[i] = base[index].ratingMean + (norm > 0 ? result / norm : 0);
